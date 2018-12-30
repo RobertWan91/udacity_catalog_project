0c3ecf4ae75829b25515e14670a34d2358831a09
Robert Wan <robertuwa@gmail.com>
2018-12-31 01:03:44 +1100

    Edit/Delete: simple completed version 1

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -11,40 +12,63 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 # Main page
-@app.route('/') # localhost: 8000 --> all categories
+@app.route('/')  # localhost: 8000 --> all categories
 def categoriesMain():
-	categories = session.query(Categories).all()
-	return render_template('mainpage.html', categories=categories)
+    categories = session.query(Categories).all()
+    return render_template('mainpage.html', categories=categories)
+
 
 # Items list page
 @app.route('/catalog/<catalog_name>/items/')
 def catagoriesItem(catalog_name):
-    output = ''
     category = session.query(Categories).filter_by(name=catalog_name).one()
-    items = session.query(Items).filter_by(cat_id = category.id)
-    return render_template('itempage.html', categories= category,items=items)
+    items = session.query(Items).filter_by(cat_id=category.id)
+    return render_template('itempage.html', categories=category, items=items)
+
 
 # Item description page
 @app.route('/catalog/<catalog_name>/<item_name>/')
 def itemdescription(catalog_name, item_name):
-    output = ''
     category = session.query(Categories).filter_by(name=catalog_name).one()
-    items = session.query(Items).filter_by(cat_id = category.id)
+    items = session.query(Items).filter_by(cat_id=category.id)
     for i in items:
         if i.title == item_name:
-	        return render_template('describepage.html', items=i)
+            return render_template('describepage.html', items=i)
+
 
 # Edit page
-@app.route('/catalog/<item_name>/edit')
+@app.route('/catalog/<item_name>/edit', methods=['GET', 'POST'])
 def editItem(item_name):
-    return 'Edit page as first phase'
+    edititem = session.query(Items).filter_by(title=item_name).one()
+    editcategory = session.query(Categories).filter_by(id=edititem.cat_id).one()
+    if request.method == 'POST':
+        if request.form['title']:
+            edititem.title = request.form['title']
+        if request.form['description']:
+            edititem.description = request.form['description']
+        session.add(edititem)
+        session.commit()
+        return redirect(url_for('itemdescription', catalog_name=editcategory.name, item_name=edititem.title))
+    else:
+        return render_template('edititem.html', catalog_name=editcategory.name, item_name=edititem.title,
+                               item_description=edititem.description)
+
 
 # Delete page
-@app.route('/catalog/<item_name>/delete')
+@app.route('/catalog/<item_name>/delete', methods=['GET', 'POST'])
 def deleteItem(item_name):
-    return 'Delete page as first phase'
+    itemToDelete = session.query(Items).filter_by(title=item_name).one()
+    categoryDelete = session.query(Categories).filter_by(id=itemToDelete.cat_id).one()
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        return redirect(url_for('catagoriesItem', catalog_name=categoryDelete.name))
+    else:
+        return render_template('deleteitem.html', catalog_name=categoryDelete.name, item_name=itemToDelete.title)
+
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run(host = '0.0.0.0', port = 8000)
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
